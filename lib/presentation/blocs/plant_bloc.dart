00a50118496406708cbd19f3a0ecdba387f289ea
Angel <angel.a.todorov@gmail.com>
2020-07-12 13:27:01 +0300

    Updated to isolate

--- lib/presentation/blocs/plant_bloc.dart
+++ lib/presentation/blocs/plant_bloc.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 import 'package:moor/moor.dart';
 import 'package:bloc_sample_app/local_data_sources/app_database.dart';
@@ -8,15 +9,24 @@ import './bloc.dart';
 import 'dart:io';
 import 'package:image/image.dart';
 
+Uint8List _makeThumbnail(Uint8List byteImage, {int width = 120}) {
+  Image img = decodeImage(byteImage);
+  Image thumbnail = copyResize(img, width: 120);
+  return encodePng(thumbnail);
+}
+
+Future<List<Uint8List>> _getImageThumbnailAsBytes(String fileName) async {
+  List<Uint8List> imageBytes = List();
+  File imageFile = File(fileName);
+  Uint8List byteImage = await imageFile.readAsBytes();
+  Uint8List byteThumbnail = _makeThumbnail(byteImage);
+  imageBytes.addAll([byteImage, byteThumbnail]);
+  return imageBytes;
+}
+
 class PlantBloc extends Bloc<PlantEvent, PlantState> {
   final PlantRepository plantRepository;
   PlantBloc({@required this.plantRepository}) : assert(plantRepository != null);
-
-  Uint8List _makeThumbnail(Uint8List byteImage, {int width = 120}) {
-    Image img = decodeImage(byteImage);
-    Image thumbnail = copyResize(img, width: 120);
-    return encodePng(thumbnail);
-  }
 
   @override
   PlantState get initialState => PlantStateInitial();
@@ -34,16 +44,13 @@ class PlantBloc extends Bloc<PlantEvent, PlantState> {
     }
     if (event is PlantEventNewImage) {
       yield PlantStateImageIsLoading(plantsCompanion: state.plantsCompanion);
-      //await Future.delayed(Duration(milliseconds: 500));
-      File imageFile = File(event.fileName);
-      Uint8List byteImage = await imageFile.readAsBytes();
-      Uint8List byteThumbnail = _makeThumbnail(byteImage);
+      List<Uint8List> byteImages = await compute(_getImageThumbnailAsBytes, event.fileName);
       PlantsCompanion plant = state.plantsCompanion.copyWith(
         image: Value(
-          byteImage,
+          byteImages[0],
         ),
         thumbnail: Value(
-          byteThumbnail,
+          byteImages[1],
         ),
       );
       yield PlantStateImageSet(
